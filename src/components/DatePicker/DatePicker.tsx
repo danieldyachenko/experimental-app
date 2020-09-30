444380b20186562ec3330acc1d1e33122341bd24
danieldyachenko <danieldyachenko@Mac-mini-danieldyachenko.local>
2020-09-30 14:37:04 +0300

    formating code

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
-import formatDate from "./helpers/formatDate";
-import validate from "./helpers/validate";
+import React, { useEffect, useRef, useState } from 'react';
+import formatDate from './helpers/formatDate';
+import validate from './helpers/validate';
 import {
     DatePickerInput,
     DatePickerRoot,
@@ -14,32 +14,32 @@ import {
     Day,
     EmptyDay,
     Error,
-} from "./styled";
-import { InputChange, Months, OnDayClick, OnMouseMove, Week } from "./types";
+} from './styled';
+import { InputChange, Months, OnDayClick, OnMouseMove, Week } from './types';
 
 const months: Months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
 ];
-const week: Week = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+const week: Week = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
 export default function DatePicker() {
     const [date, setDate] = useState<Date>(new Date());
     const [open, setOpen] = useState<boolean>(false);
     const [isAnimated, setAnimated] = useState<boolean>(false);
-    const [inputValue, setInputValue] = useState<string>("");
+    const [inputValue, setInputValue] = useState<string>('');
     const [error, setError] = useState<boolean>(false);
-    const [placeholder, setPlaceholder] = useState<string>("Select date");
+    const [placeholder, setPlaceholder] = useState<string>('Select date');
     const [selectedDate, setSelectedDate] = useState<Date>(new Date());
 
     const datePickerElement = useRef<HTMLDivElement>(null);
@@ -50,8 +50,8 @@ export default function DatePicker() {
                 !datePickerElement.current.contains(event.target as Node) &&
                 hideDropdownPanel();
         };
-        window.addEventListener("click", onClickOutsideHandler);
-        return () => window.removeEventListener("click", onClickOutsideHandler);
+        window.addEventListener('click', onClickOutsideHandler);
+        return () => window.removeEventListener('click', onClickOutsideHandler);
     }, [open, datePickerElement]);
 
     const openDropdownPanel = () => {
@@ -59,7 +59,7 @@ export default function DatePicker() {
         setOpen(true);
 
         if (inputValue && validate(inputValue)) {
-            const dateArray = inputValue.split(".");
+            const dateArray = inputValue.split('.');
             const newDate = new Date(
                 parseInt(dateArray[2]),
                 parseInt(dateArray[1]) - 1,
@@ -71,7 +71,7 @@ export default function DatePicker() {
 
     const hideDropdownPanel = () => {
         setAnimated(false);
-    }
+    };
 
     const onAnimationEnd = () => !isAnimated && setOpen(false);
 
@@ -91,7 +91,7 @@ export default function DatePicker() {
         days.push(i - emptyDaysNumber);
     }
 
-    const onDayClick: OnDayClick = (day) => {
+    const onDayClick: OnDayClick = day => {
         const selectedDate = new Date(date.getFullYear(), date.getMonth(), day);
         const newInputValue = formatDate(selectedDate);
         setInputValue(newInputValue);
@@ -101,19 +101,19 @@ export default function DatePicker() {
         setError(false);
     };
 
-    const onMouseMove: OnMouseMove = (day) => {
+    const onMouseMove: OnMouseMove = day => {
         const selectedDate = new Date(date.getFullYear(), date.getMonth(), day);
         const newPlaceholderValue = formatDate(selectedDate);
         setPlaceholder(newPlaceholderValue);
     };
 
     const onMouseLeave = () => {
-        setPlaceholder("Select date");
+        setPlaceholder('Select date');
     };
 
-    const datePickerInputChange: InputChange = (event) => {
+    const datePickerInputChange: InputChange = event => {
         if (validate(event.target.value)) {
-            const dateArray = event.target.value.split(".");
+            const dateArray = event.target.value.split('.');
             const newDate = new Date(
                 parseInt(dateArray[2]),
                 parseInt(dateArray[1]) - 1,
@@ -131,7 +131,7 @@ export default function DatePicker() {
         } else {
             setError(false);
         }
-    }
+    };
 
     return (
         <DatePickerRoot>
@@ -152,22 +152,22 @@ export default function DatePicker() {
                     onAnimationEnd={onAnimationEnd}
                 >
                     <Top>
-                        <PaginationBtn direction="left" onClick={monthAgo} />
+                        <PaginationBtn direction='left' onClick={monthAgo} />
                         <CurrentDateTitle>
                             {`${months[date.getMonth()]} ${date.getFullYear()}`}
                         </CurrentDateTitle>
                         <PaginationBtn
-                            direction="right"
+                            direction='right'
                             onClick={monthForward}
                         />
                     </Top>
                     <WeeksContainer>
-                        {week.map((week) => (
+                        {week.map(week => (
                             <WeekText key={week}>{week}</WeekText>
                         ))}
                     </WeeksContainer>
                     <DaysContainer onMouseLeave={onMouseLeave}>
-                        {days.map((day) =>
+                        {days.map(day =>
                             day >= 1 ? (
                                 <Day
                                     key={day}
